ce579a543fa060d601b983e911bc2d771dde87ef
Ofri Peretz <ofriperetzdev@gmail.com>
2026-09-20 23:38:27 -0500

    chore(release): version packages (#1055)

--- packages/eslint-plugin-react-features/src/index.ts
+++ packages/eslint-plugin-react-features/src/index.ts
@@ -240,7 +240,7 @@ withCanonicalDocsUrls('plugin-react-features', rules);
 export const plugin = {
   meta: {
     name: 'eslint-plugin-react-features',
-    version: '1.7.6',
+    version: '1.7.7',
   },
   rules,
 } satisfies TSESLint.FlatConfig.Plugin;
