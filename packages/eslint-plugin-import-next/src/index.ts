4923e938ad5c34bf2401572562c22ffb1efa4509
Ofri Peretz <ofriperetzdev@gmail.com>
2026-09-22 16:26:28 -0500

    chore(release): version packages

--- packages/eslint-plugin-import-next/src/index.ts
+++ packages/eslint-plugin-import-next/src/index.ts
@@ -185,7 +185,7 @@ withCanonicalDocsUrls('plugin-import-next', rules);
 export const plugin = {
   meta: {
     name: 'eslint-plugin-import-next',
-    version: '2.8.3',
+    version: '2.8.4',
   },
   rules,
 } satisfies TSESLint.FlatConfig.Plugin;
