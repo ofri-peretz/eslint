7c72847e6edbf1288add32aa5b47adafcc447bd3
Ofri Peretz <ofriperetzdev@gmail.com>
2026-09-22 20:15:26 -0500

    chore(release): version packages (#1099)

--- packages/eslint-plugin-modernization/src/index.ts
+++ packages/eslint-plugin-modernization/src/index.ts
@@ -38,7 +38,7 @@ withCanonicalDocsUrls('plugin-modernization', rules);
 export const plugin: TSESLint.FlatConfig.Plugin = {
   meta: {
     name: 'eslint-plugin-modernization',
-    version: '3.1.5',
+    version: '3.1.6',
   },
   rules,
 } satisfies TSESLint.FlatConfig.Plugin;
